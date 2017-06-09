db6c2b0165e06712327ecc93a589e14abebe9bf8
kiricon <kiricon@live.com>
2017-06-09 14:41:28 -0600

    Added validation with an optional "required" attribute on the paper element attribute.

--- src/components/paperinput/index.js
+++ src/components/paperinput/index.js
@@ -19,16 +19,31 @@ export default class AsyncImg extends Component {
 		border.style.width = '0%';
 	}
 
+	validateInput() {
+		if (this.props.required !== undefined) {
+
+			let input = this.base.querySelector('input');
+
+			if (input.value.trim() !== '' && input.value !== null) {
+				input.style.border = 'solid 1px green';
+			}
+			else {
+				input.style.border = 'solid 1px red';
+			}
+
+		}
+	}
+
 	render() {
 		return (
             <div class={style.inputContainer} >
                 <input class={style.paperinput}
-                    {...this.props}
-                    type="text"
-                    onFocus={this.growBorder.bind(this)}
-                    onBlur={this.shrinkBorder.bind(this)}
+	{...this.props}
+	type="text"
+	onFocus={this.growBorder.bind(this)}
+	onBlur={this.shrinkBorder.bind(this)}
+	onKeyUp={this.validateInput.bind(this)}
                 />
-                    
                 <div class={style.borderbottom} />
             </div>
 	    );
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
 				<h1>Home</h1>
 				<p>This is the Home component.</p>
 				<AsyncImg src="assets/icons/android-chrome-192x192.png" style="background:grey; height: 100px; width:100px;" />
-				<PaperInput />
+				<PaperInput required />
 			</div>
 		);
 	}
