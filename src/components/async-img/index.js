cc39ec254e4cb22b6f2a4727b5f57d0eefee74af
kiricon <kiricon@live.com>
2017-06-09 14:05:25 -0600

    Added comment to load method.

--- src/components/async-img/index.js
+++ src/components/async-img/index.js
@@ -3,6 +3,10 @@ import style from './style';
 
 export default class AsyncImg extends Component {
 
+    /**
+     * Change opacity of img tag on image load
+     * @param {Event} e - Event automatically passed by dom
+     */
 	load(e) {
 		let img = e.target;
 	    img.style.opacity='1';
